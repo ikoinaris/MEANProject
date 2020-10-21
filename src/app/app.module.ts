613849d8c8b6b3350a3f0413638dd6b2d705c9c2
ikoinaris <g.koinaris@hotmail.com>
2020-10-21 22:35:34 +0200

    use of HttpClient to fetch initial posts from Backend - Use of setHeader to solve CORS problem

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+
+// Material Modules
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
@@ -28,7 +31,8 @@ import { PostListComponent } from './posts/post-list/post-list.component'
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model'
 import { Subject } from 'rxjs'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
     private posts: Post[] = [];
     private postUpdated = new Subject<Post[]>();
 
+    constructor(private httpClient : HttpClient) {}
+
     getPosts() {
-        return [...this.posts];
+        this.httpClient.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
+        .subscribe((postData) => {
+            this.posts = postData.posts;
+            this.postUpdated.next([...this.posts]);
+        });
     }
 
     getPostUpdateListener(){
@@ -17,6 +24,7 @@ export class PostsService {
 
     addPost(title: string, content: string) {
         const post: Post = {
+            id: null,
             title: title,
             content: title
         };

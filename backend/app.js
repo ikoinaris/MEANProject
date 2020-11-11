501d21cc8bae63a2269a695a69ee4a22acf7172e
ikoinaris <g.koinaris@hotmail.com>
2020-11-11 13:51:16 +0100

    Establish connection to Database

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://john:Fl224Ci3VvYfFrBr@cluster0.fr4p4.mongodb.net/test?retryWrites=true&w=majority",
+  {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log("Connected to Database")
+  })
+  .catch(() => {
+    console.log("Connection failed!")
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));

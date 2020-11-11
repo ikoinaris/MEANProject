b361d5a82ed563744a0b7380a27f697ec098d6c4
ikoinaris <g.koinaris@hotmail.com>
2020-11-11 16:29:39 +0100

    Posts service updated

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@angular/core';
-import { Post } from './post.model'
-import { Subject } from 'rxjs'
+import { Post } from './post.model';
+import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
     private posts: Post[] = [];
     private postUpdated = new Subject<Post[]>();
 
-    constructor(private httpClient : HttpClient) {}
+    constructor(private httpClient: HttpClient) {}
 
     getPosts() {
-        this.httpClient.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-        .subscribe((postData) => {
-            this.posts = postData.posts;
+        this.httpClient.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+          .pipe(map((postData) => {
+            return postData.posts.map(post => {
+              return {
+                title: post.title,
+                content: post.content,
+                id: post._id
+              };
+            });
+          }))
+        .subscribe((transformedPosts) => {
+            this.posts = transformedPosts;
             this.postUpdated.next([...this.posts]);
         });
     }
